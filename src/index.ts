31bf3147e6cd074a5e31a24551aa489e1779e198
Doug Torrance <dtorrance@piedmont.edu>
2025-03-03 18:45:59 -0500

    Run prettier on code

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,22 @@
-import { JupyterFrontEnd, JupyterFrontEndPlugin } from "@jupyterlab/application";
-import { IEditorLanguageRegistry } from "@jupyterlab/codemirror";
-import { LanguageSupport } from "@codemirror/language";
-import { macaulay2 } from "codemirror-lang-macaulay2";
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+import { IEditorLanguageRegistry } from '@jupyterlab/codemirror';
+import { LanguageSupport } from '@codemirror/language';
+import { macaulay2 } from 'codemirror-lang-macaulay2';
 
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: "jupyterlab-macaulay2:plugin",
+  id: 'jupyterlab-macaulay2:plugin',
   autoStart: true,
-  description: "CodeMirror-based syntax highlighting for Macaulay2 code",
+  description: 'CodeMirror-based syntax highlighting for Macaulay2 code',
   requires: [IEditorLanguageRegistry],
   activate: async (app: JupyterFrontEnd, registry: IEditorLanguageRegistry) => {
     registry.addLanguage({
-      name: "Macaulay2",
-      mime: "text/x-macaulay2",
+      name: 'Macaulay2',
+      mime: 'text/x-macaulay2',
       support: new LanguageSupport(macaulay2()),
-      extensions: ["m2"],
+      extensions: ['m2']
     });
   }
 };
