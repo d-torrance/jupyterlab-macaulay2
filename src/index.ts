67b0943f4bb5d0633a6bbe26b6fec89ff1e35e1e
Doug Torrance <dtorrance@piedmont.edu>
2025-03-27 22:43:50 -0400

    Syntax highlight <code> elements in output (e.g., doc examples)

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,10 @@ import {
 } from '@jupyterlab/application';
 import { IEditorLanguageRegistry } from '@jupyterlab/codemirror';
 import { LanguageSupport } from '@codemirror/language';
-import { macaulay2 } from 'codemirror-lang-macaulay2';
+import { macaulay2 as cmMacaulay2 } from 'codemirror-lang-macaulay2';
+import hljs from 'highlight.js';
+import hljsMacaulay2 from 'highlightjs-macaulay2';
+import 'highlight.js/styles/github.css';
 
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab-macaulay2:plugin',
@@ -15,9 +18,23 @@ const plugin: JupyterFrontEndPlugin<void> = {
     registry.addLanguage({
       name: 'Macaulay2',
       mime: 'text/x-macaulay2',
-      support: new LanguageSupport(macaulay2()),
+      support: new LanguageSupport(cmMacaulay2()),
       extensions: ['m2']
     });
+
+    // syntax highlighting in output
+    hljs.registerLanguage('macaulay2', hljsMacaulay2);
+
+    const observer = new MutationObserver(() => {
+      document.querySelectorAll('code.language-macaulay2').forEach(element => {
+        const htmlElement = element as HTMLElement;
+        if (!htmlElement.dataset.highlighted) {
+          hljs.highlightElement(htmlElement);
+        }
+      });
+    });
+
+    observer.observe(document.body, { childList: true, subtree: true });
   }
 };
 
